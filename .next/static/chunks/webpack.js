b0b09795530311be1e53dde75cee476f4f43daab
Abhinandan <abhinandnn21@gmail.com>
2024-01-05 01:15:39 +0530

    fixed bugs

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3972b75463e8541f"; }
+/******/ 		__webpack_require__.h = function() { return "fb0b698432e80753"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */

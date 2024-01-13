98153e8301c65316cfdbb87374489a9279b30eb8
Abhinandan Pandey <abhinandan2210049@akgec.ac.in>
2024-01-13 19:56:57 +0530

    fixed bug in responsiveness

--- src/components/bigbutton.js
+++ src/components/bigbutton.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Bigbutton() {
   return (
-    <button className='bg-white text-black text-[1.375rem] w-[31.7rem] h-[4rem] px-6 pt-3 pb-[0.625rem] rounded-[1.25rem]'>
+    <button className='bg-white text-black text-[1.375rem] w-[80vw] sm:w-[31.7rem] h-[4rem] px-6 pt-3 pb-[0.625rem] rounded-[1.25rem]'>
 Continue
     </button>
   )

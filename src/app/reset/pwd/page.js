9c406622c754dedb7864425bd82689252381b243
Abhinandan Pandey <abhinandan2210049@akgec.ac.in>
2024-01-13 21:18:49 +0530

    fixed bug in change pwd page

--- src/app/reset/pwd/page.js
+++ src/app/reset/pwd/page.js
@@ -79,7 +79,7 @@ const passwordValid=/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*#^?&])[A-Za-z\d@$
           }}
           }
           return (
-            <div className='bg-[#1a1a1a] font-Poppins min-h-[100vh] min-w-[100vw] px-[10vw] md:px-[7.4rem] py-[2rem]'>
+            <div className='bg-[#1a1a1a] font-Poppins min-h-[100vh] min-w-[100vw] px-[10vw] md:px-[7.4rem] py-[2rem] mb-[2rem]'>
                 <div className='flex flex-row items-center justify-between'><div className='flex flex-row gap-2 justify-center items-center mob:text-[1.375rem] text-[1rem] text-[#e6e6e6]'><svg className='hidden mob:flex' width="33" height="31" viewBox="0 0 33 31" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path fill-rule="evenodd" clip-rule="evenodd" d="M19.5869 6.12437H29.156C29.508 6.12437 29.8272 5.93937 30.0077 5.63725L32.4717 1.51288C32.87 0.846325 32.3897 0 31.6133 0H3.10206C2.70651 0 2.34813 0.23315 2.18784 0.594763L0.35966 4.71913C0.0665792 5.38032 0.550228 6.12437 1.27346 6.12437H11.309L6.18637 16.5652C5.86037 17.2296 6.34403 18.0056 7.08413 18.0056H9.68913C10.3854 18.0056 10.8686 18.6995 10.627 19.3526L7.62394 27.4705C7.2338 28.5251 8.61216 29.3214 9.33089 28.4566L21.7676 13.4923C22.2866 12.8679 21.8844 11.918 21.0749 11.8561L18.1564 11.6327C17.4301 11.5771 17.005 10.7885 17.3578 10.1513L19.5869 6.12437Z" fill="#E6E6E6"/>
 </svg>
@@ -119,7 +119,7 @@ const passwordValid=/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*#^?&])[A-Za-z\d@$
     onChange={handleChange}
     name="password2" className={`w-[80vw] sm:w-[31.5rem] h-[4.5rem] peer rounded-[1.25rem] ${(!errorPassword2)?'bg-transparent border-dgr':'bg-[#190F0F] border-[#FF7E7E]'} border px-[1rem] focus:pt-[0.75rem] pr-[3rem] ${!password2? 'pt-0':'pt-[0.75rem]'} outline-none box-border`}/>
     <div className='absolute right-4 top-6 cursor-pointer' onClick={passwordShow2}>
-        <Image src={showPassword?Fa:Fahid} />
+        <Image src={showPassword2?Fa:Fahid} />
       </div> 
     <label className={`absolute pointer-events-none peer-focus:top-2 peer-focus:text-[0.875rem] ${errorPassword2?'text-err':'peer-focus:text-dgr'} ${!password2? 'top-[31.2%]': `top-2 text-dgr text-[0.875rem]`} left-[1rem]`} for="pwd">Confirm password</label>
     {errorPassword2&&<span className='text-[0.875rem] text-err absolute left-0 bottom-[-35%]'>{errorPassword2}</span>}
@@ -134,7 +134,7 @@ const passwordValid=/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*#^?&])[A-Za-z\d@$
   </form>
  </div>
     </div> 
-<div className='mt-4 absolute bottom-[6%] mob:static text-[#B3B3B3] sm:hidden min-w-[80vw] text-[1.125rem] flex justify-center text-center'>Don't have an account? <span className='text-[#e6e6e6] underline'><Link href={'/signup'}>Sign up</Link></span></div>
+<div className='mt-4 absolute bottom-[3%] mob:static text-[#B3B3B3] sm:hidden min-w-[80vw] text-[1.125rem] flex justify-center text-center'>Don't have an account? <span className='text-[#e6e6e6] underline'><Link href={'/signup'}>Sign up</Link></span></div>
 
     </div>
   )

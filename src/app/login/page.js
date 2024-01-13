01332ec1c2625fd25f59da3a992cc8f4f55630ca
Abhinandan Pandey <144912135+abhinandnn@users.noreply.github.com>
2024-01-13 16:38:54 +0530

    added responsiveness in login page

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -10,7 +10,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import checkbox from '../../../public/checkbox.svg'
 import checkbox1 from '../../../public/checkbox1.svg'
-
+import smlogo from '../../../public/smlogo.svg'
 import axios from '@/api/axios';
 import toast from 'react-hot-toast';
 import loading1 from '../../../public/loading.svg'
@@ -121,16 +121,18 @@ const handleGoogle = async (event) => {
     }}}
   
   return (
-    <div className='bg-[#1a1a1a] font-Poppins min-h-[100vh] px-[7.4rem] py-[2rem]'>
-        <div className='flex flex-row justify-between'><div className='flex flex-row gap-2 justify-center items-center text-[1.375rem] text-[#e6e6e6]'><svg width="33" height="31" viewBox="0 0 33 31" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <div className='bg-[#1a1a1a] font-Poppins min-h-[100vh] min-w-[80vw] px-[10vw] md:px-[7.4rem] py-[2rem]'>
+        <div className='flex flex-row items-center justify-between'><div className='flex flex-row gap-2 justify-center items-center mob:text-[1.375rem] text-[1rem] text-[#e6e6e6]'><svg className='hidden mob:flex' width="33" height="31" viewBox="0 0 33 31" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path fill-rule="evenodd" clip-rule="evenodd" d="M19.5869 6.12437H29.156C29.508 6.12437 29.8272 5.93937 30.0077 5.63725L32.4717 1.51288C32.87 0.846325 32.3897 0 31.6133 0H3.10206C2.70651 0 2.34813 0.23315 2.18784 0.594763L0.35966 4.71913C0.0665792 5.38032 0.550228 6.12437 1.27346 6.12437H11.309L6.18637 16.5652C5.86037 17.2296 6.34403 18.0056 7.08413 18.0056H9.68913C10.3854 18.0056 10.8686 18.6995 10.627 19.3526L7.62394 27.4705C7.2338 28.5251 8.61216 29.3214 9.33089 28.4566L21.7676 13.4923C22.2866 12.8679 21.8844 11.918 21.0749 11.8561L18.1564 11.6327C17.4301 11.5771 17.005 10.7885 17.3578 10.1513L19.5869 6.12437Z" fill="#E6E6E6"/>
 </svg>
+<Image className='mob:hidden' src={smlogo}/>
 <>TypingThunder</>
 </div>
-<div className='text-[#B3B3B3] text-[1.125rem]'>Don't have an account? <span className='text-[#e6e6e6] underline'><Link href={'/signup'}>Sign up</Link></span></div>
+<div className='text-[#B3B3B3] hidden sm:inline text-[1.125rem]'>Don't have an account? <span className='text-[#e6e6e6] underline'><Link href={'/signup'}>Sign up</Link></span></div>
+<div className='text-[0.9rem] mob:text-[1.15rem] sm:hidden cursor-pointer text-white'>Test</div>
 </div>
-    <div className='flex flex-row gap-[12.5rem]'>
-    <div className='pt-[11.7rem] w-[30rem] flex flex-col gap-[1.5rem]'>
+    <div className='flex flex-row justify-center gap-[12.5rem]'>
+    <div className='pt-[11.7rem] hidden xl:flex w-[30rem] flex-col gap-[1.5rem]'>
         <div className='text-dgr text-[2.25rem] leading-[140%] font-medium'>
         Compete with your friends in 
         <span className='text-lgr'> <Typewriter
@@ -146,7 +148,7 @@ const handleGoogle = async (event) => {
         <div className='text-[1.25rem] text-dgr font-light'>Assess and improve your typing speed with our interactive typing tests. Receive instant feedback and track your progress over time.</div>
         <button className='bg-white text-[1rem] w-[11.5rem] h-[3.375rem] px-4 py-2 rounded-2xl'>Test speed</button>
         </div>
-        <div className='flex flex-col gap-8 text-[1.125rem] pt-[4.8rem] text-white'>
+        <div className='flex flex-col gap-8 w-[80vw] sm:w-auto text-[1.125rem] pt-[4.8rem] text-white'>
           <div className='flex flex-col gap-5 text-dgr'>
             <span className='text-[2.5rem] text-lgr leading-[1.125rem]'>Login</span>
             Log in to test your typing speed with your companions.
@@ -157,7 +159,7 @@ const handleGoogle = async (event) => {
     <input type="text" required id="user"
     value={inputValue}
     onChange={handleInputChange}
-    name="user" className={`w-[31.5rem] h-[4.5rem] peer rounded-[1.25rem] ${(!error)?'bg-transparent border-dgr':'bg-[#190F0F] border-[#FF7E7E]'} border  px-[1rem] focus:pt-[0.75rem] ${!inputValue? 'pt-0':'pt-[0.75rem]'} outline-none box-border`}/> 
+    name="user" className={`w-[80vw] sm:w-[31.5rem] h-[4.5rem] peer rounded-[1.25rem] ${(!error)?'bg-transparent border-dgr':'bg-[#190F0F] border-[#FF7E7E]'} border  px-[1rem] focus:pt-[0.75rem] ${!inputValue? 'pt-0':'pt-[0.75rem]'} outline-none box-border`}/> 
     <label className={`absolute pointer-events-none peer-focus:top-2 peer-focus:text-[0.875rem] ${error?'text-err':'peer-focus:text-dgr'}  ${!inputValue? 'top-[31.2%]': `top-2  text-dgr text-[0.875rem]`} left-[1rem]`} for="user">Enter username or registered email</label>
     {error&&inputValue.length>0&&<span className='text-[0.875rem] text-err absolute left-0 bottom-[-35%]'>{error}</span>}
   </div>
@@ -165,7 +167,7 @@ const handleGoogle = async (event) => {
     <input type={showPassword?'text':'password'} required id="pwd"
     value={password}
     onChange={handlePasswordChange}
-    name="pwd" className={`w-[31.5rem] h-[4.5rem] peer rounded-[1.25rem] ${(!errorPassword)?'bg-transparent border-dgr':'bg-[#190F0F] border-[#FF7E7E]'} border px-[1rem] focus:pt-[0.75rem] pr-[3rem] ${!password? 'pt-0':'pt-[0.75rem]'} outline-none box-border`}/>
+    name="pwd" className={`w-[80vw] sm:w-[31.5rem] h-[4.5rem] peer rounded-[1.25rem] ${(!errorPassword)?'bg-transparent border-dgr':'bg-[#190F0F] border-[#FF7E7E]'} border px-[1rem] focus:pt-[0.75rem] pr-[3rem] ${!password? 'pt-0':'pt-[0.75rem]'} outline-none box-border`}/>
     <div className='absolute right-4 top-6 cursor-pointer' onClick={passwordShow}>
         <Image src={showPassword?Fa:Fahid} />
       </div> 
@@ -178,10 +180,10 @@ const handleGoogle = async (event) => {
   <Image className='hover:cursor-pointer' onClick={()=>setCheck(!isCheck)} src={isCheck?checkbox:checkbox1}/>
 Remember me</div>
   </div>
-  <button className='bg-white text-black text-[1.375rem] w-[31.7rem] h-[4rem] flex flex-row items-center justify-center px-6 pt-3 pb-[0.625rem] rounded-[1.25rem]'>
+  <button className='bg-white text-black text-[1.375rem] w-[80vw] sm:w-[31.7rem] h-[4rem] flex flex-row items-center justify-center px-6 pt-3 pb-[0.625rem] rounded-[1.25rem]'>
 {loading?<Image src={loading1} className='animate-spin'/>:"Continue"
 }</button>
-<button type='button' onClick={handleGoogle} className='bg-transparent flex flex-row justify-center items-center gap-4 border border-dgr text-[1.125rem] w-[31.7rem] h-[4rem] px-[8.56rem] py-[1rem] rounded-[1.25rem]'>
+<button type='button' onClick={handleGoogle} className='bg-transparent flex flex-row justify-center items-center gap-4 border border-dgr text-[1.125rem] w-[80vw] sm:w-[31.7rem] h-[4rem] px-[1rem] sm:px-[8.56rem] py-[1rem] rounded-[1.25rem]'>
     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 <g clip-path="url(#clip0_607_111)">
 <path fill-rule="evenodd" clip-rule="evenodd" d="M23.52 12.2728C23.52 11.4218 23.4436 10.6037 23.3018 9.81821H12V14.46H18.4582C18.18 15.96 17.3345 17.2309 16.0636 18.0818V21.0928H19.9418C22.2109 19.0037 23.52 15.9273 23.52 12.2728Z" fill="#4285F4"/>
@@ -199,6 +201,8 @@ Remember me</div>
   </form>
  </div>
     </div> 
+<div className='mt-4 text-[#B3B3B3] sm:hidden text-[1.125rem] flex justify-center text-center'>Don't have an account? <span className='text-[#e6e6e6] underline'><Link href={'/signup'}>Sign up</Link></span></div>
+
     </div>
   )
 }
